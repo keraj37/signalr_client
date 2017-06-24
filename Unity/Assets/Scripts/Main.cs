ff5d285efa754bc4891b9349b4a3ac36941f2ce1
keraj37 <jerry.switalski@gmail.com>
2017-06-24 19:10:59 +0200

    unity

---
+++ Unity/Assets/Scripts/Main.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Microsoft.AspNet.SignalR.Client;
+
+public class Main : MonoBehaviour
+{
+	void Start ()
+    {
+        var connection = new HubConnection("https://quisutdeus.in");
+        var myHub = connection.CreateHubProxy("GeneralHub");
+
+        connection.Start().ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                Console.WriteLine("There was an error opening the connection:{0}",
+                                  task.Exception.GetBaseException());
+            }
+            else
+            {
+                Console.WriteLine("Connected");
+            }
+
+        }).Wait();
+
+        myHub.Invoke<string, string>("SendChatMessage", (x => Console.WriteLine("Progress: " + x)), "JS CLient", "I am working").ContinueWith(task => {
+            if (task.IsFaulted)
+            {
+                Console.WriteLine("There was an error calling send: {0}",
+                                  task.Exception.GetBaseException());
+            }
+            else
+            {
+                Console.WriteLine(task.Result);
+            }
+        });
+
+        myHub.On<string, string>("addNewMessageToPage", (param, param2) => {
+            Console.WriteLine(param + ": " + param2);
+        });
+
+        //myHub.Invoke<string>("DoSomething", "I'm doing something!!!").Wait();
+
+        Console.Read();
+        connection.Stop();
+    }
+}
