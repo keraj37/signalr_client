ed1fec612f0e15bcb0da97f265129d94b44de943
keraj37 <jerry.switalski@gmail.com>
2017-06-24 22:43:52 +0200

    p

--- Unity/Assets/Scripts/Main.cs
+++ Unity/Assets/Scripts/Main.cs
@@ -2,65 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using Newtonsoft.Json.Linq;
-using SignalR.Client._20.Hubs;
-using System.Threading;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
-    HubConnection connection;
-    IHubProxy myHub;
+    public RawImage img;
 
     void Start()
     {
-        // uncomment below to stream debug into console
-        // Debug.Listeners.Add(new ConsoleTraceListener());
+        WebCamTexture webCamTexture = new WebCamTexture();
+        webCamTexture.Play();
 
-        // this is an optional query parameters to sent with each message
-        var query = new Dictionary<string, string>();
-        query.Add("version", "1.0");
-
-        // initialize connection and its proxy
-        HubConnection connection = new HubConnection("https://quisutdeus.in", query);
-        IHubProxy proxy = connection.CreateProxy("GeneralHub");
-
-        // subscribe to event
-        proxy.Subscribe("addNewMessageToPage").Data += data =>
+        if(webCamTexture.isPlaying)
         {
-            var _first = data[0] as JToken;
-            Debug.Log(string.Format("Received: [{0}] from {1}",
-                _first["message"].ToString(), _first["from"].ToString()));
-        };
+            Texture2D PhotoTaken = new Texture2D(webCamTexture.width, webCamTexture.height);
+            PhotoTaken.SetPixels(webCamTexture.GetPixels());
+            PhotoTaken.Apply();
 
-        connection.Closed += () => Debug.Log("Closed");
-        connection.Error += x => Debug.Log("Error: " + x.Message);
-        connection.Received += x => Debug.Log("Recived: " + x);
-        connection.Reconnected += () => Debug.Log("Reconected");
-
-        new Thread(() =>
-        {
-            connection.Start();
-        }).Start();
-
-        /*
-        var _exit = false;
-        while (!_exit)
-        {
-            switch (Console.ReadKey(true).Key)
-            {
-                case ConsoleKey.F1:
-                    Console.Write("Sending hi... ");
-                    proxy.Invoke("SendChatMessage", "JS CLient", "I am working").Finished += (sender, e) =>
-                    {
-                        Debug.Log("done");
-                    };
-                    break;
-                case ConsoleKey.Escape:
-                    _exit = true;
-                    break;
-            }
+            img.texture = PhotoTaken;
         }
-        */
     }
 }
 
